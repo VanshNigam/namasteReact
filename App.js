18519c2c29d09875aa2c7d7be16f665401335c4e
Vansh Nigam <vanshnigam8081432513.com>
2025-02-04 02:28:33 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading2 = React.createElement("h2", {class:"root2"}, "hello world from react2");
+const root2 = ReactDOM.createRoot(document.getElementById("root"));
+root2.render( heading2);
